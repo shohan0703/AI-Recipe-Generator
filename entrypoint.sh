01837ae7dcb749c6ce7e85d0a0ab08ea04365c4d
MD. Shakib Hasan <shsohan200@gmail.com>
2026-08-20 00:14:53 +0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Virtual Framebuffer চালু
+# Virtual Framebuffer
 Xvfb :99 -screen 0 1280x800x24 &
 sleep 2
 
-# Window Manager চালু
+# Window Manager
 fluxbox &
 sleep 1
 
-# আপনার Python GUI অ্যাপ চালু
-python3 main.py &
-sleep 1
-
-# VNC Server চালু
+# VNC Server
 x11vnc -display :99 -forever -shared -rfbport 5900 &
 sleep 1
 
-# Web Proxy (noVNC) চালু
+# Python GUI App
+python3 main.py &
+sleep 2
+
+# Web Proxy (Main Foreground Process)
 websockify --web=/usr/share/novnc/ 10000 localhost:5900 --default-path=vnc.html
